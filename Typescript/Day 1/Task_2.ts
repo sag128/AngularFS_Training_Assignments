620238508f9429ad3083deb6cecda65969591a44
sag128 <sagarmot6@gmail.com>
2022-04-25 13:14:56 +0530

    Added Day 1 Tasks in TS

---
+++ Typescript/Day 1/Task_2.ts
@@ -0,0 +1,9 @@
+let employeeId : number = 101;
+let employeeName : string = "Scott";
+let employeeTitle : string = "Manager";
+let isPermanentEmp : boolean = true;
+let empSalary : number = 45000.00;
+let empDeptNo : number = 10;
+
+console.log(`Employee Id : ${employeeId} \nEmployee Name : ${employeeName}\nEmployee Job : ${employeeTitle}\nIs Permanent Employee : ${isPermanentEmp}\nEmployee Salary : ${empSalary}\nEmployee Dept Number : ${empDeptNo} `);
+
