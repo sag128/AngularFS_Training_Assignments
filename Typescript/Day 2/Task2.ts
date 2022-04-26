16ce347c044245b48fe70165d0d4d06e1b0e63a1
sag128 <sagarmot6@gmail.com>
2022-04-26 15:26:26 +0530

    Added properties

--- Typescript/Day 2/Task2.ts
+++ Typescript/Day 2/Task2.ts
@@ -6,11 +6,35 @@ class OrderDetails {
     
 
 
-    constructor (id:number,productName:string,unitPrice:number,qty:number=1) {
+    constructor (id:number) {
         this.productId = id;
-        this.productName = productName;
-        this.unitPrice = unitPrice;
-        this.quantity = qty;
+        
+    }
+
+    public get ProductName() : string {
+        return this.productName;
+    }
+
+    public set ProductName(value:string) {
+        this.productName = value;
+    }
+
+    
+    public get Quantity() : number {
+        return this.quantity;
+    }
+
+    public set Quantity(value:number) {
+        this.quantity = value;
+    }
+
+    
+    public get UnitPrice() : number {
+        return this.unitPrice;
+    }
+
+    public set UnitPrice(value:number) {
+        this.unitPrice = value;
     }
 
     showDetails():void {
@@ -19,8 +43,16 @@ class OrderDetails {
 }
 
 
-let o1:OrderDetails = new OrderDetails(101,"Mi TV",14000,10);
-let o2:OrderDetails = new OrderDetails(102,"Samsung TV 4K ",70000);
+let o1:OrderDetails = new OrderDetails(101);
+o1.ProductName = "Mi TV";
+o1.Quantity=10;
+o1.UnitPrice=14000;
+
+let o2:OrderDetails = new OrderDetails(102);
+
+o2.ProductName = "Samsung TV";
+o2.Quantity=1;
+o2.UnitPrice=70000;
 
 o1.showDetails();
 o2.showDetails();
